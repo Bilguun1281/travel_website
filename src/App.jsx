fd365063eb8e499c0af1a0f3fc8d997a0055c3e4
Bilguun <bilguun11110111@gmail.com>
2025-01-17 02:21:55 +0800

    homepage

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App() {
       <Router>
         <Navbar />
         <Routes>
-        <Route path='/react_website1'  element={<Home/>} />
+        <Route path='/travel_website'  element={<Home/>} />
         <Route path='/services' element={<Services/>} />
         <Route path='/products' element={<Products/>} />
         <Route path='/sign-up' element={<SignUp/>} />
